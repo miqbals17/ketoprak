e1985a0c1f5de7ffb0b659fc0c6019f6bb62786a
Muhammad Iqbal S <37977826+miqbals17@users.noreply.github.com>
2026-03-13 05:40:03 +0700

    Merge pull request #3 from miqbals17/feat/session
    feat(session): Add local saved token and cookie

---
+++ utils/getSession.ts
@@ -0,0 +1,85 @@
+import { access, mkdir, readFile, writeFile } from "node:fs/promises";
+import { join } from "node:path";
+
+interface AccountData {
+  jcCookie: string;
+  sipgnToken: string;
+}
+
+export async function isAccountFileExists(exeDir: string) {
+  const lokasiFile = join(exeDir, "data", "account.json");
+
+  try {
+    await access(lokasiFile);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+export async function getSession(exeDir: string): Promise<AccountData | null> {
+  try {
+    const accountSessionFile = join(exeDir, "data", "account.json");
+
+    const accountSession = await readFile(accountSessionFile, "utf-8");
+    const accountData = JSON.parse(accountSession);
+
+    return accountData;
+  } catch {
+    console.log("Tidak ada session tersimpan");
+    return null;
+  }
+}
+
+export async function setSession(exeDir: string, accountData: AccountData) {
+  try {
+    const accountSessionFile = join(exeDir, "data", "account.json");
+
+    await mkdir(join(exeDir, "data"), { recursive: true });
+    await writeFile(
+      accountSessionFile,
+      JSON.stringify(accountData, null, 2),
+      "utf-8",
+    );
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function checkSipgnTokenSession(token: string): Promise<boolean> {
+  try {
+    const response = await fetch(
+      "https://sipgn-api.bgn.go.id/api/v1/sipai-dashboard/auth/me",
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    return response.ok;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function checkJCCookieSession(cookie: string): Promise<boolean> {
+  try {
+    const response = await fetch(
+      "https://console.jumpcloud.com/api/users/getSelf",
+      {
+        method: "GET",
+        headers: {
+          Cookie: cookie,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    await response.json();
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
